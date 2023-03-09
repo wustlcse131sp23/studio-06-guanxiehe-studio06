ef5d81c0641cd697ac3ac0a3357ba86b4fb528c8
hannah <hannah@localhost>
2023-03-09 12:51:36 -0600

    studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -1,5 +1,7 @@
 package studio6;
 
+import java.awt.Color;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class RecursiveMethods {
@@ -13,8 +15,23 @@ public class RecursiveMethods {
 	 */
 	public static double geometricSum(int n) {
 		
+//		public static double geometricSum(int n) {
+//			double sum = 0.0;
+//			for (int power = 1; power <= n; ++power) {
+//				sum = sum + Math.pow(0.5, power);
+//			}
+//			return sum;
+//		}
 			// FIXME compute the geometric sum for the first n terms recursively
+		if(n == 0)
 			return 0;
+		else
+		{
+			return 1/(Math.pow(2,n)) + geometricSum(n-1);
+					
+		}
+		
+			
 		
 	}
 
@@ -41,11 +58,50 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
+//	public static int[] toReversed(int[] array) {
+//		int[] reversed = new int[array.length];
+//		if (array.length > 0) {
+//			int lastIndex = array.length - 1;
+//			for (int index = 0; index <= array.length / 2; ++index) {
+//				int mirrorIndex = lastIndex - index;
+//				// note:
+//				// since we read from array and write to reversed
+//				// we do not need to use a temp variable
+//				reversed[index] = array[mirrorIndex];
+//				reversed[mirrorIndex] = array[index];
+//			}
+//		}
+//		return reversed;
+//	}
 	public static int[] toReversed(int[] array) {
+		if (array.length <= 0)
+		{
+			int[] empty = { };
+			return empty;
+		}
+		else
+		{
+		int[]reversedArray = new int[array.length];
+		return helper(array,reversedArray,0);
+		}
 		
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+	}
+	
+	public static int[] helper (int[] array, int[]reversedArray, int index)
+	{
+		int lastIndex = array.length - 1;
+		if(index > array.length/2)
+		{
+			return reversedArray;
+		}
+		else
+		{
+		int mirrorIndex = lastIndex - index;
+		reversedArray[index] = array[mirrorIndex];
+		reversedArray[mirrorIndex] = array[index];
+		return helper(array, reversedArray,index+1);
+		}
 	}
 
 	/**
@@ -57,8 +113,26 @@ public class RecursiveMethods {
 	 *                                      depth
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
+	
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
+
+		double bound = 5.0;
+		StdDraw.setXscale(-bound, bound);
+		StdDraw.setYscale(-bound, bound);
+		StdDraw.setPenColor(Color.BLACK);
+		if(radius < radiusMinimumDrawingThreshold)
+		{
+			
+		}
+		else
+		{
+			StdDraw.circle(xCenter,yCenter,radius);
+			circlesUponCircles(xCenter-radius,yCenter,radius/3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter+radius,yCenter,radius/3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter,yCenter-radius,radius/3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter,yCenter+radius,radius/3, radiusMinimumDrawingThreshold);
+		}
 		
 		// FIXME
 	}
